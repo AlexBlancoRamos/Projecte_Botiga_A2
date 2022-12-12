569cf02a459c8368f3f3da9d66fdd43f99e56284
AlexBlancoRamos <alex.blanco@institutvidreres.cat>
2022-12-12 09:57:45 +0100

    Actualització a Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PeuComponent } from './peu/peu.component';
+import { IniciComponent } from './inici/inici.component';
+import { CatalegComponent } from './cataleg/cataleg.component';
+import { CistellaComponent } from './cistella/cistella.component';
+import { CondicionsComponent } from './condicions/condicions.component';
+import { ContacteComponent } from './contacte/contacte.component';
+import { RegistreComponent } from './registre/registre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PeuComponent,
+    IniciComponent,
+    CatalegComponent,
+    CistellaComponent,
+    CondicionsComponent,
+    ContacteComponent,
+    RegistreComponent
   ],
   imports: [
     BrowserModule
