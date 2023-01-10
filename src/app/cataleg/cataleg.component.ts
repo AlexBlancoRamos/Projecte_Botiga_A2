0a9aa9ab55242d0f9f5f142160cd7a222b20f935
Valldu <gerard.valldosera@institutvidreres.ca>
2023-01-10 13:51:54 +0100

    De catalaleg a cistella

--- src/app/cataleg/cataleg.component.ts
+++ src/app/cataleg/cataleg.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./cataleg.component.css']
 })
 export class CatalegComponent {
+  toggle(event: Event): void {
+    let elementId: string = (event.target as Element).id;
 
+    if (elementId=='p1') {
+      localStorage.setItem("producte1", document.getElementById('producte1')!.innerHTML)
+    }else if (elementId=='p2'){
+      localStorage.setItem("producte2", document.getElementById('producte2')!.innerHTML)
+    }else if (elementId=='p3') {
+      localStorage.setItem("producte3", document.getElementById('producte3')!.innerHTML)
+    }else if (elementId=='p4') {
+      localStorage.setItem("producte4", document.getElementById('producte4')!.innerHTML)
+    }else if (elementId=='p5') {
+      localStorage.setItem("producte5", document.getElementById('producte5')!.innerHTML)
+    }else if (elementId=='p6') {
+      localStorage.setItem("producte6", document.getElementById('producte6')!.innerHTML)
+    }else if (elementId=='t1') {
+      localStorage.setItem("torre1", document.getElementById('torre1')!.innerHTML)
+    }else if (elementId=='t2') {
+      localStorage.setItem("torre2", document.getElementById('torre2')!.innerHTML)
+    }else if (elementId=='t3') {
+      localStorage.setItem("torre3", document.getElementById('torre3')!.innerHTML)
+    }else if (elementId=='t4') {
+      localStorage.setItem("torre4", document.getElementById('torre4')!.innerHTML)
+    }else if (elementId=='t5') {
+      localStorage.setItem("torre5", document.getElementById('torre5')!.innerHTML)
+    }else if (elementId=='t6') {
+      localStorage.setItem("torre6", document.getElementById('torre6')!.innerHTML)
+    }else if (elementId=='pe1') {
+      localStorage.setItem("periferico1", document.getElementById('periferico1')!.innerHTML)
+    }else if (elementId=='pe2') {
+      localStorage.setItem("periferico2", document.getElementById('periferico2')!.innerHTML)
+    }else if (elementId=='pe3') {
+      localStorage.setItem("periferico3", document.getElementById('periferico3')!.innerHTML)
+    }else if (elementId=='pe4') {
+      localStorage.setItem("periferico4", document.getElementById('periferico4')!.innerHTML)
+    }else if (elementId=='pe5') {
+      localStorage.setItem("periferico5", document.getElementById('periferico5')!.innerHTML)
+    }else if (elementId=='pe6') {
+      localStorage.setItem("periferico6", document.getElementById('periferico6')!.innerHTML)
+    }
+  }
 }
--- src/app/cistella/cistella.component.ts
+++ src/app/cistella/cistella.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cistella',
   templateUrl: './cistella.component.html',
   styleUrls: ['./cistella.component.css']
 })
-export class CistellaComponent {
+export class CistellaComponent implements OnInit{
+constructor() {
 
+}
+ngOnInit() {
+  const subject = document.querySelector('#subject')!;
+  if (localStorage.getItem("producte1")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("producte1")!);
+  }
+  if (localStorage.getItem("producte2")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("producte2")!);
+  }
+  if (localStorage.getItem("producte3")! == null) {
+  } else {
+  subject.insertAdjacentHTML('afterend', localStorage.getItem("producte3")!);
+}
+  if (localStorage.getItem("producte4")! == null) {
+  } else {
+  subject.insertAdjacentHTML('afterend', localStorage.getItem("producte4")!);
+}
+  if (localStorage.getItem("producte5")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("producte5")!);
+  }
+  if (localStorage.getItem("producte6")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("producte6")!);
+  }
+  if (localStorage.getItem("torre1")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre1")!);
+  }
+  if (localStorage.getItem("torre2")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre2")!);
+  }
+  if (localStorage.getItem("torre3")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre3")!);
+  }
+  if (localStorage.getItem("torre4")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre4")!);
+  }
+  if (localStorage.getItem("torre5")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre5")!);
+  }
+  if (localStorage.getItem("torre6")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("torre6")!);
+  }
+  if (localStorage.getItem("periferico1")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico1")!);
+  }
+  if (localStorage.getItem("periferico2")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico2")!);
+  }
+  if (localStorage.getItem("periferico3")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico3")!);
+  }
+  if (localStorage.getItem("periferico4")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico4")!);
+  }
+  if (localStorage.getItem("periferico5")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico5")!);
+  }
+  if (localStorage.getItem("periferico6")! == null) {
+  } else {
+    subject.insertAdjacentHTML('afterend', localStorage.getItem("periferico6")!);
+  }
+
+}
+borrar(){
+  window.localStorage.clear()
+}
 }
