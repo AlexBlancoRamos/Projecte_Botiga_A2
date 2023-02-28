d2d9a61293205b81a4b9c4613d23244df109b24d
Valldu <gerard.valldosera@institutvidreres.ca>
2023-02-28 13:38:02 +0100

    clientperf

---
+++ src/app/contrasenya/contrasenya.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-contrasenya',
+  templateUrl: './contrasenya.component.html',
+  styleUrls: ['./contrasenya.component.css']
+})
+export class ContrasenyaComponent {
+  email: any;
+  user:any;
+constructor(private http:HttpClient) {
+}
+
+enviar(){
+   this.http.post("http://172.16.5.1:3080/api/sendemail",{name:'user', email:this.email,subject:"Nova contrasenya",message:"text de prov"}).subscribe((client) => {
+   console.log(client);
+   })
+  alert('Correu enviat')
+}
+}
+
+
